35c02c12f5a0d2acf2bb2468972c11b52e0cc10e
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-20 15:36:33 +0300

    added map method

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+ let newtut=tutorials.map((tutorial)=>{
+    return tutorial
+   .split(" ")
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(" ")
+})
+  return newtut
 }
+
+console.log(tutorials);
+
+console.log(titleCased());
+
+
